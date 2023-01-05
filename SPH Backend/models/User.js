18d3f01a186dae573a6bfa6a1fb1ca87198a38ab
DESKTOP-QRHER1C\ESCOTO <justine.anicdao@gmail.com>
2023-01-06 00:59:22 +0800

    Optimizing frontend and backend

---
+++ SPH Backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+require('mongoose-type-email');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    haumail:{ 
+        type: mongoose.SchemaTypes.Email, 
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: [String],
+        default: ["Student"]
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
---
+++ SPH Frontend/src/config/roles.js
@@ -0,0 +1,6 @@
+export const ROLES = {
+    Student: 'Student',
+    Employee: 'Employee',
+    Manager: 'Manager',
+    Admin: 'Admin'
+}
