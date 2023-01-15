47330054e68bdfca8389f233bd33b8f8b249f41c
Justine Nicdao <justine.anicdao@gmail.com>
2023-01-15 19:52:03 +0800

    SPH-63-FE: Create Certificate Generation Page.

--- SPH Frontend/src/App.js
+++ SPH Frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Public from './components/Public'
-import Login from './features/auth/Login';
+import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
 import NotesList from './features/notes/NotesList'
@@ -10,12 +10,13 @@ import EditUser from './features/users/EditUser'
 import NewUserForm from './features/users/NewUserForm'
 import EditNote from './features/notes/EditNote'
 import NewNote from './features/notes/NewNote'
-import GenerateSummary from './features/generate/GenerateSummary';
+import GenerateSummary from './features/generate/GenerateSummary'
+import GenerateCertificate from './features/generate/GenerateCertificate'
 import Prefetch from './features/auth/Prefetch'
 import PersistLogin from './features/auth/PersistLogin'
 import RequireAuth from './features/auth/RequireAuth'
 import { ROLES } from './config/roles'
-import useTitle from './hooks/useTitle';
+import useTitle from './hooks/useTitle'
 
 function App() {
   useTitle('SAUP Portal HAU')
@@ -52,6 +53,9 @@ function App() {
                 <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />}>
                   <Route path="generate-summary">
                     <Route index element={<GenerateSummary />} />
+                  </Route>
+                  <Route path="generate-certificate">
+                    <Route index element={<GenerateCertificate />} />
                   </Route>
                 </Route>
                 
