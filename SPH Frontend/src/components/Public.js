971a689a57d88b517bb8448ca17cb5feb45b037a
Nicdao, Justine A <justine.anicdao@gmail.com>
2023-01-17 05:58:39 +0800

    SPH-75-FE: Adjusted Landing Page Design.

--- SPH Frontend/src/components/Public.js
+++ SPH Frontend/src/components/Public.js
@@ -3,24 +3,26 @@ import { Link } from 'react-router-dom'
 const Public = () => {
     const content = (
         <body className='ba text-white bg-rose-400/40 relative bg-blend-multiply'>
-        <section className="public">
-            <header className='text-3xl'>
-                <h1 className=''>Welcome to <span className="">Office of the Community Extension Services (OCES)!</span></h1>
-            </header>
-            <main className="public__main">
-                <p>Head, Office of the Community Extension Services and National Service Training Program</p>
-                <address className="public__addr">
-                    Ms. Shirley Mae B. Marcos<br />
-                    smbarrera@hau.edu.ph<br />
-                    Monday to Friday 8:00 a.m. to 5:00 p.m.<br />
-                </address>
-                <br />
-            </main>
-            <footer>
-                <Link to="/login">
-                    <button className='decoration-white bg-sky-400 rounded-md text-2xl'>Employee Login</button>
-                </Link>
-            </footer>
+        <section className="m-20 mt-10">
+            <div className="">
+                <main className="bg-white/80 text-black font-semibold shadow-lg sm:rounded-lg border public__main">
+                <div className="text-3xl px-10 p-5 font-bold text-red-900">Welcome to <span>Office of the Community Extension Services (OCES)!</span></div>
+                    <p className="px-10 py-5">Head, Office of the Community Extension Services and National Service Training Program</p>
+                    <address className="px-10 pb-5">
+                        Ms. Shirley Mae B. Marcos<br />
+                        smbarrera@hau.edu.ph<br />
+                        Monday to Friday 8:00 a.m. to 5:00 p.m.<br />
+                    </address>
+                    <footer className="float-right mt-32 mr-10">
+                        <Link to="/login">
+                        <button className="text-white bg-red-900 hover:bg-red-800 font-semibold rounded-lg text-base px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
+                            User Login
+                        </button>
+                        </Link>
+                    </footer>
+                    <br/><br/><br/><br/><br/><br/>
+                </main>
+            </div>
         </section>
         </body>
     )
