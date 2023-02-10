c38c2b41eab8e7f117da758d7b1609ea5149c172
Nicdao, Justine A <justine.anicdao@gmail.com>
2023-02-11 01:13:28 +0800

    SPH-84-FE: Added Search option in Data Overview Page and Adjusted Partition of Infos.

--- SPH Frontend/src/features/outreach/OutreachList.js
+++ SPH Frontend/src/features/outreach/OutreachList.js
@@ -47,20 +47,20 @@ const OutreachList = () => {
         <>
 
         <p className="text-2xl font-semibold">Outreach Projects
-        <p className="text-sm font-bold float-right">
-        <button className="pr-2">    
-            <FontAwesomeIcon icon={faNavicon} />
-        </button>View
- </p>
-
+            <p className="text-sm font-bold float-right">
+                <button className="pr-2">    
+                    <FontAwesomeIcon icon={faNavicon} />
+                </button>View
             </p>
-            <nav class="shadow-md shadow-gray-400 mb-4 p-3 border-gray-200 rounded bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <div class="flex flex-wrap items-center justify-between mx-auto">
-                <ul class="flex  gap-x-20  mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        </p>
+
+            <nav className="shadow-md shadow-gray-400 mb-4 p-3 border-gray-200 rounded bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex flex-wrap items-center justify-between mx-auto">
+                <ul className="flex gap-x-20 mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                     <li>
-                    <label className=" px-4 py-10 text-sm font-bold">What are you looking for?</label>
-                    <input
-                        type="text" placeholder="Search" className=" z-1 block ml-4 bg-gray-300 border py-1 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-900 focus:border-rose-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div className=" px-4 text-sm font-bold">What are you looking for?</div>
+                        <input
+                            type="text" placeholder="Search" className=" z-1 block ml-4 bg-gray-300 border py-1 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-900 focus:border-rose-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         </input>
                     </li>
                     <li>
