9b1c6870feb9ffcb6d03010398701c51ad183e33
Justine Nicdao <justinenicdao05@gmail.com>
2023-04-22 00:00:25 +0800

    Changed Submit Report into Submit Form on New  Employee and Student Outreach.

--- SPH Frontend/src/features/forms/NewEmployeeOutreach.js
+++ SPH Frontend/src/features/forms/NewEmployeeOutreach.js
@@ -530,7 +530,7 @@ const NewEmployeeOutreach = () => {
                             className="text-white inline-flex bg-red-900 hover:bg-red-800 font-medium rounded-lg text-sm px-4 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
                             title="Save"
                           >
-                            Submit Report
+                            Submit Form
                           </button>
                         </div>
                       </div>
--- SPH Frontend/src/features/forms/NewStudentOutreach.js
+++ SPH Frontend/src/features/forms/NewStudentOutreach.js
@@ -764,7 +764,7 @@ const content = (
                     className="text-white inline-flex bg-red-900 hover:bg-red-800 font-medium rounded-lg text-sm px-4 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
                     title="Save"
                   >
-                    Submit Report
+                    Submit Form
                   </button>
                 </div>
               </div>
